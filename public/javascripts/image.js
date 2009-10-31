c507e4ebeb89a3fad497f90e2c5380492b74f346
Kevin McPhillips <kevin@gleep.ca>
2009-10-31 12:24:27 -0500

    Upload overlay working. Updated jquery version.

---
+++ public/javascripts/image.js
@@ -0,0 +1,11 @@
+function show_upload() {
+  $("#upload").overlay({
+    top: 272,
+    expose: {
+      color: '#000000',
+      loadSpeed: 100,
+      opacity: 0.3
+    },
+    api: true
+  }).load();
+}
