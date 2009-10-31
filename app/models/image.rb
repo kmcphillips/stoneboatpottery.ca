06d748ec8ddd5d61da01eb6ee07fa20d1137b928
Kevin McPhillips <kevin@gleep.ca>
2009-10-31 09:56:15 -0500

    Delete images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,17 +1,21 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
 
+  before_destroy :delete_images
+
   has_attachment :content_type => :image,
                  :storage => :file_system,
                  :path_prefix => "public/images/attachment/",
-                 :resize_to => '320x200>',
-                 :thumbnails => { :thumb => '100x100>' }
+                 :resize_to => '700x700>',
+                 :thumbnails => { :thumb => '250x250>' }
   
 #  validates_presence_of :imageable
-  validates_presence_of :filename, :content_type, :size, :height, :width
+  validates_presence_of :filename, :content_type, :size
 #  validates_as_attachment
   
   attr_protected :id
+
+  named_scope :full, :conditions => "thumbnail IS NULL"
 
   def make_primary
     raise StandardError, "Must be associated to an object to make it the default" unless self.imageable
@@ -20,4 +24,13 @@ class Image < ActiveRecord::Base
     end
     self.update_attiribute(:primary, true)
   end
+
+protected
+
+  def delete_images
+    Image.all(:conditions => ["parent_id = ?", self.id]).each {|img| img.destroy }
+    
+    FileUtils.rm(self.full_filename)
+  end
+  
 end
