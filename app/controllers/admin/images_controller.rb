ae115863b664c5cee36d4e4b55a06becc45fb563
Kevin McPhillips <kevin@gleep.ca>
2009-11-07 14:32:45 -0600

    Finished upload for single image, except layout

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,45 +1,86 @@
 class Admin::ImagesController < ApplicationController
-  before_filter :require_login
-
-  def index
-    @images = Image.all
-  end
+  before_filter :require_login, :lookup_imageable
 
   def new
     @image = Image.new
   end
 
   def create
-    @image = Image.new(params[:image])
+    respond_to do |wants|
+      wants.js do
+        @image = Image.new(params[:image])
 
-    responds_to_parent do
-      if @image.save
-        begin
-          @imageable = params[:imageable_type].constantize.find(params[:imageable_id])
-          if @imageable.respond_to?(:image)
-            @imageable.image.andand.destroy   # This puts exceptions in the logs but works so i'm just going to leave it as-is
-            @imageable.image = @image
-          elsif @imageable.respond_to?(:images)
-            @imageable.images << @image
+        responds_to_parent do
+          if @image.save
+            begin
+              if @imageable.respond_to?(:image)
+                @imageable.image.andand.destroy   # This puts exceptions in the logs but works so i'm just going to leave it as is
+                @imageable.image = @image
+              elsif @imageable.respond_to?(:images)
+                @imageable.images << @image
+              else
+                raise StandardError, "does not respond to image or images"
+              end
+
+              flash.now[:notice] = 'Image successfully added.'
+            rescue => e
+              flash.now[:error] = 'Unable to associate image to object. Contact administrator.'
+              # TODO log this exception
+            end
           else
-            raise StandardError, "does not respond to image or images"
+            flash.now[:error] = @image.errors.full_messages.to_sentence
           end
 
-          flash.now[:notice] = 'Image successfully added.'
-        rescue => e
-          flash.now[:error] = 'Unable to associate image to object. Contact administrator.'
-          raise e
+          render :update do |page|
+            page.call "upload_after"
+            page.replace_html :image_container, :partial => "shared/image_container", :locals => {:imageable => @imageable}
+            page.replace_html :flashes, :partial => "shared/flashes"
+          end
         end
-      else
-        flash.now[:error] = @image.errors.full_messages.to_sentence
-      end
-
-      render :update do |page|
-        page.call "upload_after"
-        page.replace_html :image_container, :partial => "shared/image_container", :locals => {:imageable => @imageable}
-        page.replace_html :flashes, :partial => "shared/flashes"
       end
     end
   end
 
+  def destroy
+    respond_to do |wants|
+      wants.js do
+        @image = Image.find_by_id(params[:id])
+        imageable = @image.imageable if @image
+
+        render :update do |page|
+          if imageable.respond_to?(:image)
+            if @image.destroy
+              flash.now[:notice] = "Image removed successfully."
+            else
+              flash.now[:error] = @image.errors.full_messages.to_sentence
+            end
+
+            imageable.reload
+            page.replace_html :image_container, :partial => "shared/image_container", :locals => {:imageable => imageable}
+          elsif imageable.respond_to?(:images)
+
+          else
+            flash.now[:error] = "Unable to find image. Contact administrator."
+            # TODO log weird error here
+          end
+
+          page.replace_html :flashes, :partial => "shared/flashes"
+        end
+      end
+    end
+  end
+
+  def make_primary
+    respond_to do |wants|
+      wants.js do
+        
+      end
+    end
+  end
+
+protected
+
+  def lookup_imageable
+    @imageable = params[:imageable_type].constantize.find(params[:imageable_id]) rescue nil
+  end
 end
