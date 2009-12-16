59378c328e3f7afb2573bedfa17a4cf3a0be115d
Kevin McPhillips <kimos-github@gleep.ca>
2009-12-16 06:42:38 -0600

    Fixed bug where password could be set to an empty string

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,18 +10,26 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     
-    if params[:password] && params[:password_confirm]
-      if params[:password] == params[:password_confirm]
-        if @user.update_attributes(:password_hash => User.encrypt(params[:password]))
-          flash[:notice] = "Password successfully changed."
+    if @user == current_user
+      if params[:password] && params[:password_confirm]
+        if params[:password] == params[:password_confirm]
+          if params[:password].length >= 4
+            if @user.update_attributes(:password_hash => User.encrypt(params[:password]))
+              flash[:notice] = "Password successfully changed."
+            else
+              flash[:error] = @user.errors.full_messages.to_sentence
+            end
+          else
+            flash[:error] = "Password must be at least four characters."
+          end
         else
-          flash[:error] = @user.errors.full_messages.to_sentence
-        end      
+          flash[:error] = "Passwords do not match."
+        end
       else
-        flash[:error] = "Passwords do not match."
+        flash[:error] = "Password was not passed for update."
       end
     else
-      flash[:error] = "Password was not passed for update."
+      flash[:error] = "You can only update your own password."
     end
 
     redirect_to edit_admin_user_path(@user)
