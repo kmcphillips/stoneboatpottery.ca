d182066e91153a23192654856d21d1702a046a8f
Kevin McPhillips <kimos-github@gleep.ca>
2010-02-13 11:44:34 -0600

    Fixed bug from andand refactor.

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -2,7 +2,7 @@ class SubcategoriesController < ApplicationController
 
   def show
     @category = Category.find_by_permalink(params[:category_id])
-    @subcategory = @category.subcategories.find_by_permalink(params[:id]) if @subcategory
+    @subcategory = @category.subcategories.find_by_permalink(params[:id]) if @category
 
     if @subcategory && (@subcategory.inherited_active? || current_user)
       flash[:warning] = "This subcategory is not active and can't be seen by users other than you." unless @subcategory.active?
