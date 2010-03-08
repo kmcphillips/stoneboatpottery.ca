3edcd3136c8abf7ae2d71f98bfd36a55c0f318d0
Kevin McPhillips <kimos-github@gleep.ca>
2010-03-07 22:01:46 -0600

    Fixed multiple things for JoAnna. Fixed populates, proboblems in image naming. Updated the header image. Kept feature list up to date

--- db/populate/blocks.rb
+++ db/populate/blocks.rb
@@ -1,8 +1,8 @@
 Block.connection.execute("truncate blocks;")
 
 blocks = [
-          {:label => "about_joanna", :section_path => "/about_joanna", :accepts_image => true},
-          {:label => "about_stoneboat", :section_path => "/about_stoneboat", :accepts_image => true},
+          {:label => "about_joanna", :section_path => "/about", :accepts_image => true},
+          {:label => "about_stoneboat", :section_path => "/about", :accepts_image => true},
           {:label => "contact", :section_path => "/contact", :accepts_image => false},
           {:label => "wholesale", :section_path => "/wholesale", :accepts_image => false}
         ]
