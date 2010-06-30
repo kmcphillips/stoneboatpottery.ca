31b0ad6e4145db3cdaec830637487b37c76d0bd8
Kevin McPhillips <github@kevinmcphillips.ca>
2010-06-30 13:46:50 -0500

    Configured app to deploy with capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+set :application, "Stoneboat Pottery"
+set :repository,  "git://github.com/kimos/stoneboat_pottery.git"
+set :deploy_to, "/home/kevin/stoneboatpottery.ca"
+set :user, "kevin"
+set :use_sudo, false
+set :scm, "git"
+set :keep_releases, 10
+
+
+# server "production", :app, :web, :db, :primary => true
+role :web, "gleep.ca"                          # Your HTTP server, Apache/etc
+role :app, "gleep.ca"                          # This may be the same as your `Web` server
+role :db,  "gleep.ca", :primary => true        # This is where Rails migrations will run
+
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+task :after_update_code do
+  run "ln -s #{shared_path}/images #{release_path}/public/images/attachment"
+
+  %w{database.yml}.each do |config|
+    run "ln -s #{shared_path}/#{config} #{release_path}/config/#{config}"
+  end
+end
+
