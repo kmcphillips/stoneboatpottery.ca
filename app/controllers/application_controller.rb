ec3e5e3b7d6a30b150af96af49365b2942b22e01
Kevin McPhillips <github@kevinmcphillips.ca>
2017-01-31 22:05:01 -0500

    This error class no longer exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
   unless StoneboatpotteryCa::Application.config.consider_all_requests_local
     rescue_from Exception, :with => :render_500
     rescue_from ActionController::RoutingError, :with => :render_404
-    rescue_from ActionController::UnknownAction, :with => :render_404
     rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   end
 
